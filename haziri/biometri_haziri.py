cb243e3c59199a1321ed74b7c3100879d82ef440
mohammadbilalniazi <mohammadbilalniazi2016@gmail.com>
2024-01-11 03:26:50 -0800

    dkd

--- haziri/biometri_haziri.py
+++ haziri/biometri_haziri.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from logs.models import FaceLog
 from hawala.date_changing import current_shamsi_date
 from datetime import datetime
-from hawala.models import Controller,Mudeeriath
+from hawala.models import Mudeeriath
 from .models import MONTHS
 from django.db.models import Q
 from django.contrib.auth.models import User
@@ -27,38 +27,34 @@ def daily_haziri_report(request,mudeeriath_id=None,user=None,year=None,month=Non
     if year==None or month==None:
         year=current.split('-')[0]
         month=current.split('-')[1]
-        logs=FaceLog.objects.filter(date__range=[initial_date,current_date])
+        # logs=FaceLog.objects.filter(date__range=[initial_date,current_date])
         daily_haziries=Daily_Haziri.objects.filter(date__range=[initial_date,current_date])
         # print("initial_date,current_date ",logs)
     else:
-        logs=FaceLog.objects.filter(year=int(year),month=int(month))
+        # logs=FaceLog.objects.filter(year=int(year),month=int(month))
         initial_date=datetime.strptime(str(year)+"-"+str(month)+"-01",'%Y-%m-%d')
         last_date=datetime.strptime(str(year)+"-"+str(month)+"-31",'%Y-%m-%d')
         daily_haziries=Daily_Haziri.objects.filter(date__range=[initial_date,last_date])
         # print("year=int(year),month=int(month) ",logs)
     if mudeeriath_id!=None:  
-        logs=logs.filter(profile__user__controller__mudeeriath__id=mudeeriath_id)
+        # logs=logs.filter(profile__user__controller__mudeeriath__id=mudeeriath_id)
         daily_haziries=daily_haziries.filter(user__controller__mudeeriath_id=mudeeriath_id)
-        # print("2 mudeeriath_id ",logs," daily_haziries",daily_haziries)
     else:
         mudeeriath_id=0
-        # print("2 mudeeriath_id=0")
 
     if user!=None:  
-        logs=logs.filter(profile__user__id=user)
+        # logs=logs.filter(profile__user__id=user)
         daily_haziries=daily_haziries.filter(user__id=user)
-        # print("3 user!=None ",logs," daily_haziries",daily_haziries)
-        
     else:
         user=request.user.id
-    print("logs ",logs," daily_haziries",daily_haziries)
+    # print("logs ",logs," daily_haziries",daily_haziries)
     
     mudeeriaths=Mudeeriath.objects.all()
     users=User.objects.filter(Q(mudeeriath__id=mudeeriath_id) | Q(controller__mudeeriath__id=mudeeriath_id))
     months=[{"value":month[0],"label":month[1]} for month in MONTHS]
     # print("logs ",logs," mudeeriaths ",mudeeriaths)
     # print("daily_haziries.log.set ",daily_haziries[3].log_set.all())
-    context['logs']=logs
+    # context['logs']=logs
     context['daily_haziries']=daily_haziries
     context['mudeeriath_id']=int(mudeeriath_id)
     context['month']=int(month)
--- static/assets/js/controller/controllers_haziri.js
+++ static/assets/js/controller/controllers_haziri.js
@@ -1,12 +1,8 @@
 function controllers_haziri() {
-
     mudeeriaths = document.getElementById("mudeeriath");
     start_date_input = document.getElementById("start_date_input");
-    // end_date_input = document.getElementById("end_date_input");
-
     mudeeriath_id = mudeeriaths.value;
     start_date_input = start_date_input.value;
-    // end_date_input = end_date_input.value;
     console.log("mudeeriaths.value=", mudeeriath_id);
 
     url = "/haziri/controller/haziri/" + mudeeriath_id + "/" + change_date(start_date_input) + "/";
@@ -16,139 +12,47 @@ function controllers_haziri() {
         })
         .then((response) => response.json())
         .then((data) => {
-            const monthly_haziri =
-                document.querySelector("#monthly_haziri");
-            // console.log("data=",data);
+            const monthly_haziri = document.querySelector("#monthly_haziri");
             monthly_haziri.innerHTML = "";
-            // console.log(data)
-            class ControllerHaziri {
-                constructor(
-                    no,
-                    basth,
-                    first_name,
-                    father_name,
-                    mudeeriath_name,
-                    id,
-                    is_haziri_uploaded,
-                    kaifyath_haziri,
-                    mudeeriath,
-                    report_date,
-                    user,
-                    user_name,
-                    wazeefa,
-                    month,
-                    total_present,
-                    total_absent,
-                    total_leave,
-                    total_tafrihi, total_zaroori, total_marizi, total_waladi, total_hajj
-                ) {
-                    this.no = no;
-                    this.id = id;
-                    this.first_name = first_name;
-                    this.father_name = father_name;
-                    this.mudeeriath_name = mudeeriath_name;
-                    this.user = user;
-                    this.user_name = user_name;
-                    this.mudeeriath = mudeeriath;
-                    this.wazeefa = wazeefa;
-                    this.basth = basth;
-                    this.report_date = report_date;
-                    this.is_haziri_uploaded = is_haziri_uploaded;
-                    this.kaifyath_haziri = kaifyath_haziri;
-                    this.month = month;
-                    this.total_leave = total_leave;
-                    this.total_absent = total_absent;
-                    this.total_present = total_present;
-                    this.total_tafrihi = total_tafrihi;
-                    this.total_zaroori = total_zaroori;
-                    this.total_marizi = total_marizi;
-                    this.total_waladi = total_waladi;
-                    this.total_hajj = total_hajj;
-                }
-
-                addHtml() {
-                    let html1 = `<tr role="row" class="even">
-                      <th class="field-id">${this.no}</th>
-                      <td class="field-first_name">${this.first_name}</td>                
-                      <td class="field-first_name">${this.father_name}</td>    
-                      <td class="field-basth">${this.basth}</td>
-                      <td class="field-wazeefa">${this.wazeefa}</td>
-                      <td class="field-first_name">${this.mudeeriath_name}</td>   
-                          <td class="field-last_name"><input type="hidden" class="user" disabled style="font-weight:bolder" value="${this.user}" name="user"></td>
-                          <td class="field-id_card"><input type="hidden"  name="mudeeriath" disabled style="font-weight:bolder" class="mudeeriath" value="${this.mudeeriath}"></td>
-                          <td class="field-pdo"><input type="number" class="total_present" value="${this.total_present}" name="total_present"><input type="hidden"  class="month" value="${this.month}" name="month"></td>
-                          <td class="field-pdo"><input type="number"  class="total_absent" value="${this.total_absent}" name="total_absent"></td>
-                          <td class="field-pdo"><input type="number"  class="total_leave"  value="${this.total_leave}" name="total_leave"></td> 
-                          <td class="field-pdo"><input type="number" class="total_tafrihi" value="${this.total_tafrihi}" name="total_tafrihi">
-                          <td class="field-pdo"><input type="number"  class="total_zaroori" value="${this.total_zaroori}" name="total_zaroori"></td>
-                          <td class="field-pdo"><input type="number"  class="total_marizi"  value="${this.total_marizi}" name="total_marizi"></td> 
-                          <td class="field-pdo"><input type="number"  class="total_waladi"  value="${this.total_waladi}" name="total_waladi"></td> 
-                          <td class="field-pdo"><input type="number"  class="total_hajj"  value="${this.total_hajj}" name="total_hajj"></td> 
-                          <td><input type="text" name="kaifyath_haziri" class="kaifyath_haziri" style="font-weight:bolder"  value="${this.kaifyath_haziri}"></td>
-                          <td><input type="hidden" name="report_date" class="report_date" disabled  value="${this.report_date}"></td>`;
-
-
-                    if (this.is_haziri_uploaded == true) {
-                        //   console.log("true");
-                        // mudeeriaths.style.backgroundColor="#28a745";
-                        var html2 = `<td> <span class="alert alert-success" style="padding:0.7rem 1.0rem;font-size:100%;background-color:rgb(130 227 130)">${this.is_haziri_uploaded}</span></td>`;
-                    } else {
-                        //   console.log("false");
-                        // mudeeriaths.style.backgroundColor="#f24734";
-                        var html2 = `<td  ><span class="alert alert-danger"  style="padding:0.7rem 1.0rem;font-size:100%">${this.is_haziri_uploaded}</span></td>`;
-                    }
-
-                    // mudeeriaths.style.color="white";
-                    let html3 = `<td>status</td></tr>`;
-                    monthly_haziri.insertAdjacentHTML(
-                        "beforeend",
-                        html1 + html2
-                    );
-                }
-            }
-
-            let mudeeriaths_list = [];
             for (key in data) {
                 no = key + 1;
-                mudeeriaths_list.push(
-                    new ControllerHaziri(
-                        key,
-                        data[key]["basth"],
-                        data[key]["first_name"],
-                        data[key]["father_name"],
+                let html1 = `<tr role="row" class="even">
+                <th class="field-id">${no}</th>
+                <td class="field-first_name">${data[key]["first_name"]}</td>                
+                <td class="field-first_name">${data[key]["father_name"]}</td>    
+                <td class="field-basth">${data[key]["basth"]}</td>
+                <td class="field-wazeefa">${data[key]["wazeefa"]}</td>
+                <td class="field-first_name">${data[key]["mudeeriath_name"]}</td>   
+                    <td class="field-last_name"><input type="hidden" class="user" disabled style="font-weight:bolder" value="${data[key]["user"]}" name="user"></td>
+                    <td class="field-id_card"><input type="hidden"  name="mudeeriath" disabled style="font-weight:bolder" class="mudeeriath" value="${data[key]["mudeeriath"]}"></td>
+                    <td class="field-pdo"><input type="number" class="total_present" value="${data[key]["total_present"]}" name="total_present"><input type="hidden"  class="month" value="${data[key]["month"]}" name="month"></td>
+                    <td class="field-pdo"><input type="number"  class="total_absent" value="${data[key]["total_absent"]}" name="total_absent"></td>
+                    <td class="field-pdo"><input type="number"  class="total_leave"  value="${data[key]["total_leave"]}" name="total_leave"></td> 
+                    <td class="field-pdo"><input type="number" class="total_tafrihi" value="${data[key]["total_tafrihi"]}" name="total_tafrihi">
+                    <td class="field-pdo"><input type="number"  class="total_zaroori" value="${data[key]["total_zaroori"]}" name="total_zaroori"></td>
+                    <td class="field-pdo"><input type="number"  class="total_marizi"  value="${data[key]["total_marizi"]}" name="total_marizi"></td> 
+                    <td class="field-pdo"><input type="number"  class="total_waladi"  value="${data[key]["total_waladi"]}" name="total_waladi"></td> 
+                    <td class="field-pdo"><input type="number"  class="total_hajj"  value="${data[key]["total_hajj"]}" name="total_hajj"></td> 
+                    <td><input type="text" name="kaifyath_haziri" class="kaifyath_haziri" style="font-weight:bolder"  value="${data[key]["kaifyath_haziri"]}"></td>
+                    <td><input type="hidden" name="report_date" class="report_date" disabled  value="${data[key]["report_date"]}"></td>`;
 
-                        data[key]["mudeeriath_name"],
-                        data[key]["id"],
-                        data[key]["is_haziri_uploaded"],
-                        data[key]["kaifyath_haziri"],
-                        data[key]["mudeeriath_id"],
-                        data[key]["report_date"],
-                        data[key]["user"],
-                        data[key]["user_name"],
-                        data[key]["wazeefa"],
-                        data[key]["month"],
-                        data[key]["total_present"],
-                        data[key]["total_absent"],
-                        data[key]["total_leave"],
-                        data[key]["total_tafrihi"],
-                        data[key]["total_zaroori"],
-                        data[key]["total_marizi"],
-                        data[key]["total_waladi"],
-                        data[key]["total_hajj"]
-                    )
+
+                if (data[key]["is_haziri_uploaded"] == true) {
+                    var html2 = `<td> <span class="alert alert-success" style="padding:0.7rem 1.0rem;font-size:100%;background-color:rgb(130 227 130)">${data[key]["is_haziri_uploaded"]}</span></td>`;
+                } else {
+                    var html2 = `<td  ><span class="alert alert-danger"  style="padding:0.7rem 1.0rem;font-size:100%">${data[key]["is_haziri_uploaded"]}</span></td>`;
+                }
+
+                monthly_haziri.insertAdjacentHTML(
+                    "beforeend",
+                    html1 + html2
                 );
-
-
                 monthly_haziri_status = document.getElementById("monthly_haziri_status");
                 haziri_status = document.getElementById("haziri_status");
                 haziri_status.value = data[key]['haziri_status'];
                 monthly_haziri_status.value = data[key]['monthly_haziri_status'];
             }
-            mudeeriaths_list.forEach((mudeeriath) =>
-                mudeeriath.addHtml()
-            );
         });
-    //})
     return;
 }
 
